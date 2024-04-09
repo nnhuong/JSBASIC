f2e16f992769f3a1f300deef50a57ed068f1f082
nnhuong <nnhuong1763@gmail.com>
2024-04-09 14:25:29 +0700

    first commit

---
+++ bai23/video23.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML')
+console.log(1+2+3+4+5)
+
+const array1 = [1, 30, 4, 21, 100000]
+let arr = []
+array1.sort((item1, item2) =>{
+    if (item1 > item2) arr.push(item1)
+    console.log(arr)
+return item1 - item2;
+});
+console.log('check array1: ', array1);
+
+console.log(item1)
